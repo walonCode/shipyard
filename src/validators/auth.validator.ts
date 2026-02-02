7b78c58d3f80f0af60537086aca667d24aeb84ca
walonCode <mohamedlaminwalonjalloh@gmail.com>
2026-02-02 23:09:39 +0000

    add the auth validator

---
+++ src/validators/auth.validator.ts
@@ -0,0 +1,12 @@
+import z from "zod"
+
+export const loginSchema = z.object({
+  email: z.email().lowercase(),
+  password:z.string().min(8)
+})
+
+export const signupSchema = z.object({
+  username: z.string().min(1),
+  email: z.email().lowercase(),
+  password:z.string().min(8)
+})
